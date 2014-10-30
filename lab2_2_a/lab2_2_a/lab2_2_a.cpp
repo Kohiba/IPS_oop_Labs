de599266c8c951b5a974ddd346b3b62088839814
Kohiba <skiff-tiv@yandex.ru>
2014-10-30 14:25:44 +0300

    add lab2_2

---
+++ lab2_2_a/lab2_2_a/lab2_2_a.cpp
@@ -0,0 +1,61 @@
+// lab2_2_a.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "stdlib.h"
+
+int StrToInt(const char * str, bool & err)
+{
+	char * pLastChar = NULL;
+	int param = strtol(str, &pLastChar, 10);
+	err = ((*str == '\0') || (*pLastChar != '\0'));
+	return param;
+}
+
+bool checkInput(int argc, char * argv[], int  & temp)
+{
+	if (argc <= 1)
+	{
+		printf("Program changes the order of binary bits in the input number to the opposite. \n");
+		return false;
+	}
+
+	bool isNotNum = false;
+	const char * pArg = argv[1];
+	temp = StrToInt(pArg, isNotNum);
+	if (isNotNum)
+	{
+		printf("parameter - not the number \n");
+		return false;
+	}
+	else
+	{
+		if ((temp < 0) || (temp > 255))
+		{
+			printf("Alert! Parameter < 0  or > 255");
+			return false;
+		}
+	}
+	return true;
+}
+
+int main(int argc, char* argv[])
+{
+	int numberForExperiments = 0;
+	if (!checkInput(argc, argv, numberForExperiments))
+	{
+		return 1;
+	}
+	int tempNum = 0;
+	for (int i = 0; i < 8; i++)
+	{
+		tempNum <<= 1;
+		tempNum |= (numberForExperiments & 1);
+		numberForExperiments >>= 1;
+	}
+	numberForExperiments = tempNum;
+	printf("%d", numberForExperiments);
+	return 0;
+}
+
+ 
