7ad6a2644dab2612e020a52006b00edca6578651
Kohiba <skiff-tiv@yandex.ru>
2014-11-21 16:42:51 +0300

    add new versus lab 2_1, 2_2 2_3

--- lab2_3_b/lab2_3_b/lab2_3_b.cpp
+++ lab2_3_b/lab2_3_b/lab2_3_b.cpp
@@ -6,9 +6,11 @@
 #include "iostream"
 #include "errno.h"
 #include "string"
-//#include "iostream.h"
-#include "iomanip" 
+#include "iomanip"
+#include "fstream"
+
 using namespace std;
+typedef double matrix[3][3];
 
 double StringToDouble(const char * str, bool & err)
 {
@@ -17,48 +19,44 @@ double StringToDouble(const char * str, bool & err)
 	err = ((*str == '\0') || (*pLastChar != '\0'));
 	return param;
 }
-bool DoCheckParameter(int argc, int i, char * argv[], FILE **pFile)
+bool ReadMatrix(const char *fileName, matrix & m)
 {
-	if (argc <= i)
-	{
-		cout << "error. NOT parameters" << endl << "for example : input1.txt input2.txt " << endl;
-		return false;
-	}
-	char *pArg = argv[i];
-	char *point = strchr(pArg, '.');
-	if (point == NULL)
-	{
-		cout << "error! Filename incorrectly. Enter a file name such as \"input.txt\"" << endl;
-		return false;
-	}
-	errno_t err = fopen_s(pFile, pArg, "rb");
+	FILE *pFile = NULL;
+	errno_t err = fopen_s(&pFile, fileName, "rb");
 	if (err)
 	{
 		cout << "error! Fail specified was not found" << endl;
 		return false;
 	}
-	return true;
-}
-void ParseFileIntoArray(FILE **fileName, double matr1[3][3])
-{
-	char load_string[250];
+	char load_string[200];
 	int i = 0;
 	int j = 0;
 	char *next;
 	bool isNotNum = false;
-	while (fgets(load_string, sizeof(load_string), *fileName))
+
+	while (fgets(load_string, sizeof(load_string), pFile))
 	{
-		char *str = strtok_s(load_string, "	", &next);
+		cout << load_string << endl;
+	    char *str = strtok_s(load_string, "	", &next);
 		do
 		{
-			matr1[i][j] = StringToDouble(str, isNotNum);
+			m[i][j] = StringToDouble(str, isNotNum);
+			/*if (!isNotNum)
+			{
+				cout << "error! Is not numeric" << endl;
+				return false;
+			}*/
 			j++;
-		} while (str = strtok_s(NULL, "	", &next));
+			
+		}
+		while (str = strtok_s(NULL, " ", &next));
 		i++;
 		j = 0;
 	}
+	fclose(pFile);
+	return true;
 }
-void MatrixMultiplication(double matr1[3][3], double matr2[3][3], double resultMatr[3][3])
+void MatrixMultiplication(const matrix m1, const matrix m2, matrix & rezm)
 {
 	for (int row = 0; row < 3; row++)
 	{
@@ -66,19 +64,21 @@ void MatrixMultiplication(double matr1[3][3], double matr2[3][3], double resultM
 		{
 			for (int inner = 0; inner < 3; inner++)
 			{
-				resultMatr[row][col] += matr1[row][inner] * matr2[inner][col];
+				rezm[row][col] += m1[row][inner] * m2[inner][col];
+				cout << m1[row][inner] << " : " << m2[inner][col] << " : " << rezm[row][col] << endl;
+				//printf("%f : %f : %f", m1[row][inner], m2[inner][col], rezm[row][col]);
 			}
 		}
 	}
 }
-void OutputMatrix(double matr[3][3])
+void OutputMatrix(const matrix m)
 {
 	for (int i = 0; i <= 2; i++)
 	{
 		for (int j = 0; j <= 2; j++)
 		{
-			printf("%10.3f", matr[i][j]);
-			//cout << setw(6) << setprecision(3) << resultMatr[i][j] << " ";
+			//printf("%f", m[i][j]);
+			cout << setw(6) << setprecision(3) << m[i][j] << " ";
 		}
 		cout << endl;
 	}
@@ -86,24 +86,20 @@ void OutputMatrix(double matr[3][3])
 
 int main(int argc, char* argv[])
 {
-	FILE *matrix = NULL;
-	int parameterCount = 1;
-	double matr1[3][3] = { 0 };
-	if (!DoCheckParameter(argc, parameterCount, argv, &matrix))
+	if (argc != 3)
+	{
+		cout << "error. NOT parameters" << endl << "for example : input1.txt input2.txt " << endl;
+		return 1;
+	}
+	matrix m1, m2;
+	if (!ReadMatrix(argv[1], m1) || !ReadMatrix(argv[2], m2))
 	{
 		return 1;
 	}
-	ParseFileIntoArray(&matrix, matr1);
-	double matr2[3][3] = { 0 };
-	parameterCount++;
-	if (!DoCheckParameter(argc, parameterCount, argv, &matrix))
-	{
-		return 1;
-	}
-	ParseFileIntoArray(&matrix, matr2);
-	double resultMatr[3][3] = {0};
-	MatrixMultiplication(matr1, matr2, resultMatr);
-	OutputMatrix(resultMatr);
-	return 0;
+	matrix resultMatrix;
+	MatrixMultiplication(m1, m2, resultMatrix);
+	cout << endl << endl;
+	OutputMatrix(resultMatrix);
+    return 0;
 }
 
