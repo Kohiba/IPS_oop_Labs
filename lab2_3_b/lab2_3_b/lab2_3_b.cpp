40361d4a756136969c1e3f047ae80b1cbefa9381
Kohiba <skiff-tiv@yandex.ru>
2014-12-02 22:34:57 +0300

    add

--- lab2_3_b/lab2_3_b/lab2_3_b.cpp
+++ lab2_3_b/lab2_3_b/lab2_3_b.cpp
@@ -10,7 +10,7 @@
 #include "fstream"
 
 using namespace std;
-typedef double matrix[3][3];
+typedef double Matrix3x3[3][3];
 
 double StringToDouble(const char * str, bool & err)
 {
@@ -19,7 +19,8 @@ double StringToDouble(const char * str, bool & err)
 	err = ((*str == '\0') || (*pLastChar != '\0'));
 	return param;
 }
-bool ReadMatrix(const char *fileName, matrix & m)
+
+bool ReadMatrix(const char *fileName, Matrix3x3 & m)
 {
 	FILE *pFile = NULL;
 	errno_t err = fopen_s(&pFile, fileName, "rb");
@@ -29,56 +30,65 @@ bool ReadMatrix(const char *fileName, matrix & m)
 		return false;
 	}
 	char load_string[200];
-	int i = 0;
-	int j = 0;
+	int row = 0;
+	int column = 0;
 	char *next = NULL;
 	char* token = NULL;
-	char delims[] = " ,\t\r\n";
-	bool isNotNum = false;
-
+	static const char delims[] = " ,\t\r\n";
+	
+	memset(m, 0, sizeof(m));
 	while (fgets(load_string, sizeof(load_string), pFile))
 	{
 		token = strtok_s(load_string, delims, &next);
 		while (token != NULL)
 		{
-			m[i][j] = StringToDouble(token, isNotNum);
+			bool isNotNum = false;
+			m[row][column] = StringToDouble(token, isNotNum);
 			if (isNotNum)
 			{
 				cout << "error! Is not numeric" << endl;
 				return false;
 			}
-			j++;
+			column++;
 			token = strtok_s(NULL, delims, &next);
-		};		
-		i++;
-		j = 0;
+			if (column == 3)
+			{
+				break;
+			}
+		}
+		row++;
+		column = 0;
+		if (row == 3)
+		{
+			break;
+		}
 	}
 	fclose(pFile);
 	return true;
 }
-void MatrixMultiplication(const matrix m1, const matrix m2, matrix & rezm)
+
+void MatrixMultiplication(const Matrix3x3 m1, const Matrix3x3 m2, Matrix3x3 & rezm)
 {
+	memset(rezm, 0, sizeof(rezm));
 	for (int row = 0; row < 3; row++)
 	{
 		for (int col = 0; col < 3; col++)
 		{
 			for (int inner = 0; inner < 3; inner++)
 			{
-				//cout << rezm[row][col] << " + ";
 				rezm[row][col] = rezm[row][col] + (m1[row][inner] * m2[inner][col]);
-				//cout << m1[row][inner] << " * " << m2[inner][col] << " = " << rezm[row][col] << endl;
 			}
 		}
 	}
 }
-void OutputMatrix(const matrix m)
+
+void OutputMatrix(const Matrix3x3 m)
 {
-	for (int i = 0; i <= 2; i++)
+	for (int i = 0; i < 3; i++)
 	{
-		for (int j = 0; j <= 2; j++)
+		for (int j = 0; j < 3; j++)
 		{
 			printf("%10.3f", m[i][j]);
-			//cout << setw(6) << setprecision(3) << m[i][j] << " ";
 		}
 		cout << endl;
 	}
@@ -91,13 +101,13 @@ int main(int argc, char* argv[])
 		cout << "error. NOT parameters" << endl << "for example : input1.txt input2.txt " << endl;
 		return 1;
 	}
-	matrix m1 = {0};
-	matrix m2 = {0};
+	Matrix3x3 m1;
+	Matrix3x3 m2;
 	if (!ReadMatrix(argv[1], m1) || !ReadMatrix(argv[2], m2))
 	{
 		return 1;
 	}
-	matrix resultMatrix = {0};
+	Matrix3x3 resultMatrix;
 	MatrixMultiplication(m1, m2, resultMatrix);
 	OutputMatrix(resultMatrix);
     return 0;
